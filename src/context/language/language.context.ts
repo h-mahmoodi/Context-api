8687347b5f27b18cb99ed50c7c958bb48ebc5d87
Hesam-Mahmoodi <hesamahmoodi@gmail.com>
2025-09-24 22:47:57 +0200

    feat: update language context

--- src/context/language/language.context.ts
+++ src/context/language/language.context.ts
@@ -1,6 +1,4 @@
 import { createContext } from "react";
 import type { LanguageContextValue } from "./language.type";
 
-export const LanguageContext = createContext<LanguageContextValue | undefined>(
-  undefined
-);
+export const LanguageContext = createContext<LanguageContextValue | undefined>(undefined);
--- src/context/language/language.provider.tsx
+++ src/context/language/language.provider.tsx
@@ -1,19 +1,38 @@
-import { useCallback, useState, type FC, type PropsWithChildren } from "react";
+import { useCallback, useEffect, useMemo, useState, type FC, type PropsWithChildren } from "react";
 import { Language } from "./language.type";
 import { LanguageContext } from "./language.context";
 
+const STORAGE_KEY = "language";
+
+const initialLanguageState = () => {
+  if (typeof window === "undefined") return Language.EN;
+  const fromStorage = window.localStorage.getItem(STORAGE_KEY);
+  if (fromStorage === Language.FA || fromStorage === Language.EN) return fromStorage;
+  const fromNavigator = navigator.language;
+  if (fromNavigator) return fromNavigator.toLowerCase().startsWith(Language.FA) ? Language.FA : Language.EN;
+  return Language.EN;
+};
+
 export const LanguageProvider: FC<PropsWithChildren> = ({ children }) => {
-  const [language, setLanguage] = useState<Language>(Language.EN);
+  const [language, setLanguage] = useState<Language>(initialLanguageState);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      try {
+        window.localStorage.setItem(STORAGE_KEY, language);
+      } catch {
+        console.log(`Save Language Error`);
+      }
+    }
+  }, [language]);
 
   const toggleLanguage = useCallback(() => {
-    return setLanguage((prev) =>
-      prev === Language.FA ? Language.EN : Language.FA
-    );
+    return setLanguage((prev) => (prev === Language.FA ? Language.EN : Language.FA));
   }, []);
 
-  return (
-    <LanguageContext.Provider value={{ language, setLanguage, toggleLanguage }}>
-      {children}
-    </LanguageContext.Provider>
-  );
+  const value = useMemo(() => {
+    return { language, setLanguage, toggleLanguage };
+  }, [language, toggleLanguage]);
+
+  return <LanguageContext.Provider value={value}>{children}</LanguageContext.Provider>;
 };
